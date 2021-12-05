8a65a7b71b27cd308a334f2234fe372073d8f69b
Lucas Kennedy <lukakfp@gmail.com>
2021-12-04 21:03:27 -0300

    Criado configuração do menu do layout

---
+++ components/Layout/menu.js
@@ -0,0 +1,24 @@
+const menu = [
+  {
+    name: "Home",
+    link: "/",
+    icon: "ri:compass-discover-line",
+  },
+  {
+    name: "Filmes",
+    link: "/filmes",
+    icon: "si-glyph:reel-film",
+  },
+  {
+    name: "Séries",
+    link: "/series",
+    icon: "si-glyph:television",
+  },
+  {
+    name: "Pesquisar",
+    link: "#",
+    icon: "jam:search",
+  },
+];
+
+export default menu;
