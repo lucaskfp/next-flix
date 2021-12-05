5702aa6b6faaa2742edc54d52bd79911eb9a883e
Lucas Kennedy <lukakfp@gmail.com>
2021-12-04 21:04:31 -0300

    Atualização windicss

--- windi.config.js
+++ windi.config.js
@@ -1,8 +1,24 @@
+import pluginIcons from "@windicss/plugin-icons";
 import { defineConfig } from "windicss/helpers";
+import typography from "windicss/plugin/typography";
 
 export default defineConfig({
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+  },
   extract: {
     include: ["**/*.{jsx,tsx,css}"],
     exclude: ["node_modules", ".git", ".next"],
   },
+  darkMode: "media",
+  attributify: true,
+  plugins: [
+    typography({
+      dark: true,
+    }),
+    pluginIcons,
+  ],
 });
