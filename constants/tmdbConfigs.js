58672354926be48869e7457fa2ca74cd400fa7ab
Lucas Kennedy <lukakfp@gmail.com>
2021-12-06 01:28:02 -0300

    Correção variavel de ambiente

--- constants/tmdbConfigs.js
+++ constants/tmdbConfigs.js
@@ -1,7 +1,7 @@
 const getTMDBConfigs = async () => {
   try {
     const response = await fetch(
-      `${process.env.TMDB_BASE_URL}/configuration?api_key=${process.env.TMDB_API}`
+      `${process.env.TMDB_BASE_URL}/configuration?api_key=${process.env.TMDB_KEY}`
     );
     const responseData = await response.json();
 
